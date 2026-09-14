4f5f7662da990f21df43a6014c0284516d8718c5
konstantinkovalev <konstkov@metropolia.fi>
2026-09-14 10:28:29 +0300

    Lab4 got led blinking, parser doesn't fully work yet

--- uart/uart_test.cpp
+++ uart/uart_test.cpp
@@ -20,8 +20,10 @@
 #include "pico/util/queue.h"
 #include <stdatomic.h>
 
+#define LED 20
 #define S_TO_MS 1000
-#define INACTIVITY_TIMER_PERIOD pdMS_TO_TICKS( 500 )
+
+#define INACTIVITY_TIMER_PERIOD pdMS_TO_TICKS( 30 * S_TO_MS )
 #define LED_TIMER_PERIOD pdMS_TO_TICKS( 5 * S_TO_MS )
 
 bool flush_flag = false;
@@ -53,11 +55,15 @@ static void prvInactivityTimerCallback(TimerHandle_t xTimer )
 
 static void prvLedTimerCallback(TimerHandle_t xTimer )
 {
-    gpio_put(LED, true);
+    gpio_put(LED, !gpio_get(LED));
 }
 
-void command_interpreter(const char *buf, TimerHandle_t xTimer)
+void command_interpreter(const char *buf, int count, TimerHandle_t xTimer)
 {
+    for (int i = 0; i < strlen(buf); ++i)
+    {
+      printf("Buf inside command_int: %c\n", buf[i]);
+    }
     if (strncmp(buf, "help", 4) == 0)
     {
         printf("Display usage instructions\n");
@@ -85,30 +91,46 @@ void command_interpreter(const char *buf, TimerHandle_t xTimer)
     }
 }
 
+void flush_buf(char *buf)
+{
+    for (int i = 0; i < strlen(buf); ++i)
+    {
+        buf[i] = 0;
+    }
+}
+
 void serial_task(void *param)
 {
     auto xInactivityTimer = *(TimerHandle_t*) param;
     PicoOsUart u(0, 0, 1, 115200);
     uint8_t buffer[64];
+    char str[64];
+    int char_count = 0;
     while (true)
     {
         if (int count = u.read(buffer, 64, 30); count > 0)
         {
+            //++char_count;
+            printf("Char read: %d\n", *buffer);
+            str[char_count++] = *buffer;
+            printf("Count: %d\n", char_count);
+            if (*buffer == '\r')
+            {
+                str[char_count] = '\0'; // null terminate
+                printf("Enter pressed.\n");
+                command_interpreter(str, count, xInactivityTimer);
+                flush_buf(str); // always flush the buffer after calling command interpreter
+            }
             u.write(buffer, count);
             /* When a character is received the inactivity timer is started/reset. */
             if( xTimerStart( xInactivityTimer, 0 ) != pdPASS )
             {
                 printf("The timer could not be set into the Active state.\n");
             } // When enter is pressed the received character are processed in a command interpreter
-            if (buffer[count] == '\r' || buffer[count] == '\n')
-            {
-                command_interpreter((const char*)buffer, xInactivityTimer);
-            }
-
-        } // If no characters are received in 30 seconds all the characters received so far are discarded
+        } //if no characters are received in 30 seconds all the characters received so far are discarded
         if (flush_flag)
         {
-            u.flush();
+            flush_buf(str);
             flush_flag = false;
         }
     }
@@ -118,6 +140,13 @@ void serial_task(void *param)
 FreeRTOS uart driver. **/
 int main()
 {
+    stdio_init_all();
+
+    printf("\nBoot\n");
+
+    gpio_init(LED);
+    gpio_set_dir(LED, GPIO_OUT);
+
     /* The program creates two timers */
     TimerHandle_t xTimers[2];
 
@@ -138,19 +167,17 @@ int main()
             printf("The timer was not created.\n");
             return 1;
         }
-        // else
-        // {
-        //     /* Start the timer. No block time is specified, and
-        //        even if one was it would be ignored because the RTOS
-        //        scheduler has not yet been started. */
-        //     if( xTimerStart( x, 0 ) != pdPASS )
-        //     {
-        //         printf("The timer could not be set into the Active state.\n");
-        //     }
-        // }
     }
 
-    xTaskCreate(serial_task, "producer", 512, xTimers[0], tskIDLE_PRIORITY + 1, nullptr);
+    /* Start the LED timer. No block time is specified, and
+           even if one was it would be ignored because the RTOS
+           scheduler has not yet been started. */
+    if( xTimerStart( xTimers[1], 0 ) != pdPASS )
+    {
+        printf("The timer could not be set into the Active state.\n");
+    }
+
+    xTaskCreate(serial_task, "producer", 512, &xTimers, tskIDLE_PRIORITY + 1, nullptr);
     //xTaskCreate(serial_task, "producer", 512, nullptr, tskIDLE_PRIORITY + 1, nullptr);
 
     vTaskStartScheduler();
